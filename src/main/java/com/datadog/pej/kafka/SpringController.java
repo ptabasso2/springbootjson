f4f3b89cb1300c1cf8a1543b5df58e8c1f23dc68
ptabasso2 <pejman.tabassomi@gmail.com>
2020-06-06 21:32:31 +0200

    First commit

---
+++ src/main/java/com/datadog/pej/kafka/SpringController.java
@@ -0,0 +1,21 @@
+package com.datadog.pej.kafka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SpringController {
+
+    Logger log = LoggerFactory.getLogger("commonLogger");
+
+    @RequestMapping("/test")
+    public String index() {
+        System.out.println("Instructor");
+        log.info("Instructor");
+        return "\ntest";
+    }
+
+
+}
---
+++ src/main/java/com/datadog/pej/kafka/SpringKafkaApplication.java
@@ -0,0 +1,34 @@
+package com.datadog.pej.kafka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class SpringKafkaApplication {
+
+  Logger log = LoggerFactory.getLogger("commonLogger");
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringKafkaApplication.class, args);
+  }
+
+  @Bean
+  public RestTemplate restTemplate(){
+    return new RestTemplateBuilder().build();
+  }
+
+  @Bean
+  public CommandLineRunner run() {
+    return args -> {
+        System.out.println("Instructor");
+        log.info("\ntest");
+    };
+  }
+
+}
